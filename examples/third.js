841b7c87bf7f8c3ce1f21f8252d77f0974765fa5
Olli Vanhoja <olli.vanhoja@gmail.com>
2017-08-08 22:57:53 +0300

    Update examples and add an example for pausing

---
+++ examples/third.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+// Usage ./third.js < VERY_LARGE_FILE
+
+const Sema = require('../index.js')
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+})
+
+function pause() {
+  console.log('Pausing the stream')
+}
+
+function resume() {
+  console.log('Resuming the stream')
+}
+
+const s = new Sema(5, { pauseFn: pause, resumeFn: resume })
+async function parse(line) {
+  await s.v()
+
+  console.log(line)
+
+  s.p()
+}
+
+rl.on('line', (line) => {
+  parse(line).catch(console.error)
+})
