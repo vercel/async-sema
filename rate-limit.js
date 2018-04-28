25e2ea879c8cad62a1a35b0ef34fa25e0540dce3
Olli Vanhoja <olli.vanhoja@gmail.com>
2018-04-28 06:21:52 +0300

    Add options for rate limit
    - Option to change the time unit of a rate limit on creation
      (`timeUnit`)
    - Option to change from pass max requests at once to equal request
      distribution over time (`equalDistribution`)

--- rate-limit.js
+++ rate-limit.js
@@ -1,10 +1,11 @@
 const Sema = require('./index')
 
-module.exports = function rateLimit(rps) {
-  const sema = new Sema(rps);
+module.exports = function rateLimit(rps, { timeUnit = 1000, equalDistribution = false } = {}) {
+  const sema = new Sema(equalDistribution ? 1 : rps);
+  const delay = equalDistribution ? timeUnit / rps : timeUnit;
 
   return async function rl() {
     await sema.acquire();
-    setTimeout(() => sema.release(), 1000);
+    setTimeout(() => sema.release(), delay);
   }
 }
